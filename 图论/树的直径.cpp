cd3b433dc4665cadc8cf24e307e82436919d501d
dbywsc <dbywsc@outlook.com>
2026-03-22 14:23:49 +0800

    Update 树的直径.cpp

--- 图论/树的直径.cpp
+++ 图论/树的直径.cpp
@@ -29,3 +29,49 @@ int main(void) {
 	cout << dis[B] << endl;
 	return 0;
 }
+
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+const int N = 2e5 + 10;
+vector<int> G[N];
+int n;
+int ans = 0;
+bool root[N];
+int d1[N], d2[N];
+void dfs(int u, int fa) {
+    d1[u] = d2[u] = 0;
+    for(int v : G[u]) {
+        if(v == fa) continue;
+        dfs(v, u);
+        int t = d1[v] + 1;
+        if(t > d1[u]) d2[u] = d1[u], d1[u] = t;
+        else if(t > d2[u]) d2[u] = t;
+    }
+    ans = max(ans, d1[u] + d2[u]);
+}
+void solve(void) {
+    memset(G, 0, sizeof(G));
+    memset(root, 0, sizeof(root));
+    cin >> n;
+    for(int i= 1; i < n; i++) {
+        int u, v; cin >> u >> v;
+        G[u].push_back(v); G[v].push_back(u);
+        root[v] = 1;
+    }
+    ans = 0;
+    int r = 0;
+    for(int i = 0; i < n; i++) if(!root[i]) {
+        r = i; break;
+    }
+    dfs(1, 0);
+    cout << ans << endl;
+}
+int main(void) {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    int t = 1;
+    //cin >> t;
+    while(t--) solve();
+    return 0;
+}
